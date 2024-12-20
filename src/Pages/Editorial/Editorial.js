26b28606ee7adf874eea6646e7a23772cca87f6e
lexiontechsolution <lexiontechsolution@gmail.com>
2024-12-20 15:23:09 +0530

    Update Editorial.js

--- src/Pages/Editorial/Editorial.js
+++ src/Pages/Editorial/Editorial.js
@@ -52,57 +52,62 @@ const Editorial = () => {
         </div>
       </div>
 
-      <h2>
-        National <span style={{ color: "blue" }}>Editorial </span>Advisory
-        <span style={{ color: "blue" }}> Board</span>
-      </h2>
-      <p>
-        Our National Editorial Advisory Board comprises distinguished academics
-        from premier institutions across India. They contribute insights and
-        uphold the quality of scholarship within IJEAE.
-      </p>
+      <div className="editorial-content">
+        <h2>
+          National <span style={{ color: "blue" }}>Editorial </span>Advisory
+          <span style={{ color: "blue" }}> Board</span>
+        </h2>
+        <p>
+          Our National Editorial Advisory Board comprises distinguished
+          academics from premier institutions across India. They contribute
+          insights and uphold the quality of scholarship within IJEAE.
+        </p>
 
-      <div class="container-editorial">
-        <div class="row-editorial">
-          <div class="box-editorial">
-            <p>Dr. Joseph Mathew</p>
+        <div class="container-editorial">
+          <div class="row-editorial">
+            <div class="box-editorial">
+              <p>Dr. Joseph Mathew</p>
+            </div>
+            <div class="box-editorial">
+              <p>Prof. Prasanth Arokia Samy D</p>
+            </div>
           </div>
-          <div class="box-editorial">
-            <p>Prof. Prasanth Arokia Samy D</p>
+          <div class="row-editorial">
+            <div class="box-editorial">
+              <p>Prof. Caro Velma J</p>
+            </div>
+            <div class="box-editorial">
+              <p>Dr. B. S. Gomathi</p>
+            </div>
           </div>
-        </div>
-        <div class="row-editorial">
-          <div class="box-editorial">
-            <p>Prof. Caro Velma J</p>
-          </div>
-          <div class="box-editorial">
-            <p>Dr. B. S. Gomathi</p>
-          </div>
-        </div>
-        <div class="row-editorial">
-          <div class="box fifth-box-edit">
-            <p>Dr. Cynthiya Rose J S</p>
+          <div class="row-editorial">
+            <div class="box fifth-box-edit">
+              <p>Dr. Cynthiya Rose J S</p>
+            </div>
           </div>
         </div>
       </div>
 
-      <h2>
-        International <span style={{ color: "blue" }}>Editorial </span> Advisory{" "}
-        <span style={{ color: "blue" }}>Board</span>
-      </h2>
-      <p>
-        IJEAE is committed to a global perspective on research and scholarship.
-        Our International Editorial Advisory Board includes respected scholars
-        from around the world who bring diverse expertise to the journal,
-        ensuring the relevance and impact of our published work across borders.
-      </p>
-      <div class="container-editorial">
-        <div class="row-editorial">
-          <div class="box-editorial">
-            <p style={{ textAlign: "left" }}>Dr. Sreejith Ramachandran</p>
-          </div>
-          <div class="box-editorial">
-            <p>Dr. Gamaya K P</p>
+      <div className="editorial-content">
+        <h2>
+          International <span style={{ color: "blue" }}>Editorial </span>{" "}
+          Advisory <span style={{ color: "blue" }}>Board</span>
+        </h2>
+        <p>
+          IJEAE is committed to a global perspective on research and
+          scholarship. Our International Editorial Advisory Board includes
+          respected scholars from around the world who bring diverse expertise
+          to the journal, ensuring the relevance and impact of our published
+          work across borders.
+        </p>
+        <div class="container-editorial">
+          <div class="row-editorial">
+            <div class="box-editorial">
+              <p style={{ textAlign: "left" }}>Dr. Sreejith Ramachandran</p>
+            </div>
+            <div class="box-editorial">
+              <p>Dr. Gamaya K P</p>
+            </div>
           </div>
         </div>
       </div>
