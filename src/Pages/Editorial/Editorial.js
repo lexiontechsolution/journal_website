2662d148fd142d555383dc3ced987f45c8daa5e8
lexiontechsolution <lexiontechsolution@gmail.com>
2025-03-23 20:44:30 +0530

    Update Editorial.js

--- src/Pages/Editorial/Editorial.js
+++ src/Pages/Editorial/Editorial.js
@@ -36,7 +36,7 @@ const Editorial = () => {
           process.
         </p>
         <h2>
-          <span style={{ color: "blue" }}>Editorial </span>Members
+          <span style={{ color: "blue" }}>Editorial  Board  </span>Members
         </h2>
         <div class="container-editorial">
           <div class="row-editorial">
