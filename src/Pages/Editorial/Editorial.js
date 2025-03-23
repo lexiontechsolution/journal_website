9bc9a3b2be05e6d59944d034971af1ffa49f760d
lexiontechsolution <lexiontechsolution@gmail.com>
2025-03-23 21:58:56 +0530

    Update Editorial.js

--- src/Pages/Editorial/Editorial.js
+++ src/Pages/Editorial/Editorial.js
@@ -115,20 +115,22 @@ Nehru Arts and Science College, Coimbatore, Tamil Nadu- 641105, India.<br></br
               </p>
             </div>
                   </div>
- <div class="row-editorial">
-            <div class="box-editorial">
-              <p>
-                <span style={{ fontWeigDr. P. Sasikumarht: "bold", fontStyle: "normal" }}>
-                  
-                </span>
-                <br></br>Assistant Professor of English, <br></br>Department of Social Sciences
-SRM College of Agricultural Sciences
-SRM Institute of Science and Technology
-Baburayanpettai, Chengalpattu, Tamil Nadu - 603 201, India<br></br>
-                <a href="mailto:sasikump@srmist.edu.in "> sasikump@srmist.edu.in  </a>
-              </p>
-            </div>
+ <div className="row-editorial">
+  <div className="box-editorial">
+    <p>
+      <span style={{ fontWeight: "bold", fontStyle: "normal" }}>
+        Dr. P. Sasikumar
+      </span>
+      <br></br>Assistant Professor of English, <br></br>
+      Department of Social Sciences<br></br>
+      SRM College of Agricultural Sciences<br></br>
+      SRM Institute of Science and Technology<br></br>
+      Baburayanpettai, Chengalpattu, Tamil Nadu - 603 201, India<br></br>
+      <a href="mailto:sasikump@srmist.edu.in ">sasikump@srmist.edu.in</a>
+    </p>
   </div>
+</div>
+
             <div class="box-editorial">
               <p>
                 <span style={{ fontWeight: "bold", fontStyle: "normal" }}>
