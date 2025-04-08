f71d43fa0351ea0a9371751b13d31babafba00e6
lexiontechsolution <lexiontechsolution@gmail.com>
2025-04-08 18:07:52 +0530

    Update Home.js

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -27,17 +27,14 @@ const Home = () => {
       </Helmet>
       <Header />
 
-     <div className="heading-class">
-  <h1 className="title-line">
-    <span style={{ color: "blue" }}>International</span> Journal of English for
-  </h1>
-  <div className="second-line">
-    <h1>
-      Academic <span style={{ color: "blue" }}>Excellence</span>
-    </h1>
-    <span className="issn">Online ISSN 3049-2912</span>
-  </div>
-</div>
+      <div className="heading-class">
+        <span style={{ color: "blue" }}>International </span>
+        Journal of English for
+        <br></br>
+        Academic <span style={{ color: "blue" }}>Excellence</span>
+          <br></br>
+          Online ISSN 3049-2912
+      </div>
 
       <div className="heading-class-1">
         <h3>
@@ -105,5 +102,3 @@ const Home = () => {
     </>
   );
 };
-
-export default Home;
