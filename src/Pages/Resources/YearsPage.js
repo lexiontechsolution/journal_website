1a2d408cc5bd0d4b029efcc1b563a52f33f548e2
lexiontechsolution <lexiontechsolution@gmail.com>
2025-04-24 10:19:25 +0530

    Update YearsPage.js

--- src/Pages/Resources/YearsPage.js
+++ src/Pages/Resources/YearsPage.js
@@ -14,7 +14,7 @@ const YearsPage = () => {
     const fetchYears = async () => {
       try {
         const response = await fetch(
-          "https://61.2.79.154:15002/years"
+          "https://publicationbackend.onrender.com/years"
         );
         const data = await response.json();
         setYears(data); // Set the data into state
