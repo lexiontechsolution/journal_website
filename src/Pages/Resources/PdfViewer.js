ed6b6b8f4bc90e220d661767b634c1d5f9edb4f6
lexiontechsolution <lexiontechsolution@gmail.com>
2025-04-29 10:32:40 +0530

    Create PdfViewer.js

---
+++ src/Pages/Resources/PdfViewer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const PDFViewer = () => {
+  const { id } = useParams();
+
+  const pdfUrl = `https://eeman.in:15002/view-pdf/${id}`;
+
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+    <iframe
+      src={pdfUrl}
+      width="100%"
+      height="100%"
+      title="PDF Viewer"
+    ></iframe>
+  </div>
+  );
+};
+
+export default PDFViewer;
