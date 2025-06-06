53e9fbd3c46051940271ba5238c653e4c8ebda13
lexiontechsolution <lexiontechsolution@gmail.com>
2025-06-06 14:54:46 +0530

    Update SpecialpublicationsPage.js

--- src/Pages/Resources/SpecialpublicationsPage.js
+++ src/Pages/Resources/SpecialpublicationsPage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
-import "./SpecialpublicationsPage.css";
+import "./SpecialpublicationsPage.css"; // Keep this or reuse PublicationsPage.css if styles are the same
 import { Helmet } from "react-helmet";
 
 const SpecialpublicationsPage = () => {
@@ -27,6 +27,11 @@ const SpecialpublicationsPage = () => {
     }
   }, [year, volume, issue]);
 
+  const fetchPdf = (pdfId) => {
+    const pdfUrl = `https://eeman.in:15002/view-pdf/${pdfId}`;
+    window.open(pdfUrl, "_blank");
+  };
+
   return (
     <div className="special-publications-page">
       <Helmet>
@@ -40,45 +45,59 @@ const SpecialpublicationsPage = () => {
           content="International Journal, English for Academic Research, IJEAE"
         />
       </Helmet>
+
       <Header />
+
       <div className="content">
         <div className="heading-class">
-          <span style={{ color: "white", backgroundColor: "#f39c12" }}>
-            Special Issue Publications
-          </span>
-          <br></br>
-          {year}/ Volume {volume}
+          <span style={{ color: "blue" }}>Special Issue Publications</span>
+          <br />
+          {year} / Volume {volume} / Issue {issue}
         </div>
 
-            <div className="publications-container">
-  {publications.length > 0 ? (
-    publications.map((publication, index) => (
-      <div key={publication.id || index} className="publication-box">
-        <p><strong>{index + 1}. {publication.title}</strong></p>
-        <p><strong>Author:</strong> {publication.author}</p>
-        <p><strong>DOI:</strong> 
-          <a href={publication.doi} target="_blank" rel="noopener noreferrer" style={{ marginLeft: "5px" }}>
-            {publication.doi}
-          </a>
-        </p>
-        <p>
-          <a
-            href={publication.link || publication.pdf}
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ color: "#3498db", textDecoration: "underline" }}
-          >
-            View PDF
-          </a>
-        </p>
+        <div className="publications-container">
+          {publications.length > 0 ? (
+            publications.map((publication, index) => (
+              <div key={publication._id || index} className="publication-box">
+                <p>
+                  <em>By: {publication.author}</em>
+                  <br />
+                  {publication.title}
+                  <br />
+                  {publication.doi ? (
+                    <>
+                      DOI:{" "}
+                      <a
+                        href={publication.doi}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        style={{ color: "blue" }}
+                      >
+                        {publication.doi}
+                      </a>
+                      <br />
+                    </>
+                  ) : (
+                    <>
+                      DOI: <span style={{ color: "gray" }}>Not available</span>
+                      <br />
+                    </>
+                  )}
+                  <button
+                    className="pdf-button"
+                    onClick={() => fetchPdf(publication._id)}
+                  >
+                    Article PDF
+                  </button>
+                </p>
+              </div>
+            ))
+          ) : (
+            <p>No publications found for this special issue.</p>
+          )}
+        </div>
       </div>
-    ))
-  ) : (
-    <p>No publications found for this special issue.</p>
-  )}
-</div>
 
-      </div>
       <Footer />
     </div>
   );
