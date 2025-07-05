541daadfec100d0218acdf5602a433b2aff3a7df
lexiontechsolution <lexiontechsolution@gmail.com>
2025-07-05 21:04:05 +0530

    Update PdfViewer.js

--- src/Pages/Resources/PdfViewer.js
+++ src/Pages/Resources/PdfViewer.js
@@ -4,17 +4,20 @@ import { useParams } from "react-router-dom";
 const PDFViewer = () => {
   const { id } = useParams();
 
-  const pdfUrl = `https://dev.dine360.ca/backend/publications/${id}`;
+  // Corrected URL pointing to the PDF viewing endpoint
+  const pdfUrl = `https://dev.dine360.ca/backend/view-pdf/${id}`;
 
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
-    <iframe
-      src={pdfUrl}
-      width="100%"
-      height="100%"
-      title="PDF Viewer"
-    ></iframe>
-  </div>
+      <iframe
+        src={pdfUrl}
+        width="100%"
+        height="100%"
+        title="PDF Viewer"
+        frameBorder="0"
+        allowFullScreen
+      />
+    </div>
   );
 };
 
