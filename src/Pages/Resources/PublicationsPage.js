3149e46ce3805c4dd60dbe3c63f2fd543c87cb65
lexiontechsolution <lexiontechsolution@gmail.com>
2025-07-12 11:16:36 +0530

    Update PublicationsPage.js

--- src/Pages/Resources/PublicationsPage.js
+++ src/Pages/Resources/PublicationsPage.js
@@ -4,45 +4,39 @@ import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import "./PublicationsPage.css";
 import { Helmet } from "react-helmet";
-import axios from "axios";
 
 const PublicationsPage = () => {
   const { year, volume, issue } = useParams();
   const [publications, setPublications] = useState([]);
-  const [isSpecialIssue, setIsSpecialIssue] = useState(false);
 
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      // const encodedVolume = encodeURIComponent(volume);
-      // const encodedIssue = encodeURIComponent(issue);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const encodedVolume = encodeURIComponent(volume);
+        const encodedIssue = encodeURIComponent(issue);
 
-      const response = await fetch(
-        `https://dev.dine360.ca/backend/publications?year=${year}&volume=${volume}&issue=${issue}`
-      );
+        const response = await fetch(
+          `https://dev.dine360.ca/backend/publications?year=${year}&volume=${encodedVolume}&issue=${encodedIssue}`
+        );
 
-      if (response.ok) {
-        const data = await response.json();
-        setPublications(data);
-      } else {
-        console.error("Failed to fetch publications: ", response.status);
+        if (response.ok) {
+          const data = await response.json();
+          setPublications(data);
+        } else {
+          console.error("Failed to fetch publications: ", response.status);
+          setPublications([]);
+        }
+      } catch (error) {
+        console.error("Error fetching data from backend:", error);
         setPublications([]);
       }
-    } catch (error) {
-      console.error("Error fetching data from backend:", error);
-      setPublications([]);
-    }
-  };
+    };
 
-  fetchData();
-}, [year, volume, issue]);
+    fetchData();
+  }, [year, volume, issue]);
 
-
- const fetchPdf = (pdfId) => {
-    // Construct the relative URL for the PDF
+  const fetchPdf = (pdfId) => {
     const pdfUrl = `/view-pdf/${pdfId}`;
-  
-    // Open the PDF in a new tab
     window.open(pdfUrl, "_blank");
   };
 
@@ -59,11 +53,13 @@ useEffect(() => {
           content="International Journal, English for Academic Research, IJEAE"
         />
       </Helmet>
+
       <Header />
+
       <div className="content">
         <div className="heading-class">
           <span style={{ color: "blue" }}>Regular Issue Publications</span>
-          <br /> {year}/ Volume {volume}
+          <br /> {year} / Volume {volume}
         </div>
 
         <div className="publications-container">
@@ -74,7 +70,7 @@ useEffect(() => {
                   <em>By: {publication.author}</em>
                   <br />
                   {publication.title}
-                  <br></br>
+                  <br />
                   <button
                     className="pdf-button"
                     onClick={() => fetchPdf(publication._id)}
@@ -89,6 +85,7 @@ useEffect(() => {
           )}
         </div>
       </div>
+
       <Footer />
     </div>
   );
